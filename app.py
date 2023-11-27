20d1d149db7b8b9fea4ef1a61a95ecb428dd518c
Zhang Xiaoci <142374793+xxici@users.noreply.github.com>
2023-11-27 20:05:26 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+pip install flask
+
+
+# In[ ]:
+
+
+from flask import Flask,request,render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET','POST'])
+
+def index():
+    if request.method == "POST":
+        rate = float(request.form.get("rate"))
+        print(rate)
+        return(render_template("index.html", result= -50.6*rate + 90.2))
+    else:
+        return(render_template("index.html", result="Waiting for Exchange Rate......"))
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
